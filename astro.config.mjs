febf57ff70236995da180a085c38133ff34dfce5
wappon28dev <wappon_28_dev@ruru.be>
2025-02-01 07:24:26 +0900

    wip: いったん init

---
+++ astro.config.mjs
@@ -0,0 +1,109 @@
+import react from "@astrojs/react";
+import starlight from "@astrojs/starlight";
+import remarkMermaid from "@southball/remark-mermaid";
+import partytown from "@astrojs/partytown";
+import { defineConfig } from "astro/config";
+import wikiLinkPlugin from "remark-wiki-link-plus";
+import compress from "astro-compress";
+import icon from "astro-icon";
+import metaTags from "astro-meta-tags";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    metaTags(),
+    icon({
+      include: { "mdi": ["launch"], "material-symbols": ["*"] },
+      iconDir: "src/assets/icons",
+    }),
+    compress({
+      HTML: {
+        "html-minifier-terser": {
+          removeAttributeQuotes: false,
+        },
+      },
+      CSS: false,
+    }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    starlight({
+      title: "SysLearn",
+      description: "シス研として習得してほしいスキルを学ぶ蔵",
+      defaultLocale: "ja",
+      locales: {
+        root: {
+          label: '日本語',
+          lang: 'ja',
+        },
+      },
+      logo: {
+        src: "./src/assets/icons/logo/sym.svg",
+      },
+      social: {
+        "github": "https://github.com/SystemEngineeringTeam/SysLearn",
+        "x.com": "https://x.com/set_official",
+        "instagram": "https://www.instagram.com/ait.sysken",
+      },
+      components: {
+        Sidebar: "src/components/starlight/Sidebar.astro",
+      },
+      customCss: [
+        "@fontsource-variable/inter",
+        "@fontsource-variable/noto-sans-jp",
+        "src/styles/global.css",
+        "src/styles/fonts.css",
+      ],
+      sidebar: [
+        {
+          label: "ホーム",
+          items: [
+            {
+              label: "はじめに",
+              link: "/",
+            },
+            {
+              label: "困ったときは",
+              autogenerate: { directory: "help/" },
+            }
+          ],
+        },
+        {
+          label: "C言語",
+          items: [
+            {
+              label: "基礎コース",
+              autogenerate: { directory: "textbook/c-lang/beginner" },
+            },
+          ],
+        },
+        {
+          label: "Web",
+          items: [
+            {
+              label: "授業ついていくもんコース",
+              autogenerate: { directory: "textbook/web/for-classes" },
+            },
+            {
+              label: "ハッカソン出れるもんコース",
+              autogenerate: { directory: "textbook/web/web/for-hackathons" },
+            },
+          ],
+        },
+      ],
+    }),
+    react(),
+  ],
+  site: "https://learn.sysken.net",
+  image: {
+    remotePatterns: [{ protocol: "https" }, { protocol: "http" }],
+  },
+  markdown: {
+    remarkPlugins: [
+      [remarkMermaid, { themes: ["dark", "neutral"] }],
+      [wikiLinkPlugin, { markdownFolder: "src/content/docs" }],
+    ],
+  },
+});
---
+++ src/content/docs/textbook/c-lang/beginner/00--index.md
@@ -0,0 +1,7 @@
+---
+title: はじめに
+description: このコースの説明・目標を学習します！
+slug: textbook/c-lang/beginner
+---
+
+やっほー
